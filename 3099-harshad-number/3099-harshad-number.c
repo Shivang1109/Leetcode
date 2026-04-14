89fe617023a174109b1b46e2862a68f193e5ec82
Shivang Pathak <shivangpathak11@gmail.com>
2026-04-14 13:32:01 +0530

    Time: 0 ms (100%), Space: 8.5 MB (53.49%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.c
@@ -0,0 +1,15 @@
+int sumOfTheDigitsOfHarshadNumber(int x) {
+    int temp=x;
+    int sum=0;
+    while(temp>0){
+        int rem=temp%10;
+        sum+=rem;
+        temp/=10;
+    }
+    if(x%sum==0){
+        return sum;
+    }
+    else{
+        return -1;
+    }
+}
