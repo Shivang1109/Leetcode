e2a97a3050db5e210e7c12deee984004f6ebb3d5
Shivang Pathak <shivangpathak11@gmail.com>
2026-04-16 02:44:19 +0530

    Time: 6 ms (89.17%), Space: 47.3 MB (57.02%) - LeetHub

--- 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.java
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.java
@@ -3,15 +3,8 @@ class Solution {
         int n=arr.length;
         int del=n/20;
         Arrays.sort(arr);
-        int p1=0;
-        int p2=n-1;
-        for(int i=1;i<=del;i++){
-            p1++;
-            p2--;
-            
-        }
         double sum=0;
-        for(int j=p1;j<=p2;j++){
+        for(int j=del;j<n-del;j++){
             sum+=arr[j];
         }
         double res= sum/(n- 2*del);
