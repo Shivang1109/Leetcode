a6375407216fd10d3a0deacf2e4373572b92277e
Shivang Pathak <shivangpathak11@gmail.com>
2026-06-10 12:52:38 +0530

    Time: 1 ms (62.65%), Space: 44.1 MB (43.75%) - LeetHub

--- 3798-largest-even-number/3798-largest-even-number.java
+++ 3798-largest-even-number/3798-largest-even-number.java
@@ -1,9 +1,6 @@
 class Solution {
     public String largestEven(String s) {
         int index=s.lastIndexOf("2");
-        if(index==-1){
-            return "";
-        }
         return s.substring(0,index+1);
     }
 }
