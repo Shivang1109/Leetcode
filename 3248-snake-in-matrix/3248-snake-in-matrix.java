a5e0aa1f439b3a83b46862dad18b3a67adf508ea
Shivang Pathak <shivangpathak11@gmail.com>
2026-06-11 00:44:08 +0530

    Time: 2 ms (91.58%), Space: 46.2 MB (85.96%) - LeetHub

---
+++ 3248-snake-in-matrix/3248-snake-in-matrix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int finalPositionOfSnake(int n, List<String> commands) {
+        int i=0;
+        int j=0;
+        for(String command : commands) {
+            if(command.equals("DOWN")) {
+                i++;
+            }
+            else if(command.equals("RIGHT")) {
+                j++;
+            }
+            else if(command.equals("UP")) {
+                i--;
+            }
+            else if(command.equals("LEFT")) {
+                j--;
+            }
+        }
+        return i*n+j;
+    }
+}
