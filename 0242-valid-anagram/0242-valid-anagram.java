6959bd5fe616ada206baa6e8ea470357bc78e358
Shivang Pathak <shivangpathak11@gmail.com>
2026-06-19 12:26:42 +0530

    Time: 14 ms (26.86%), Space: 46.6 MB (15.64%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for(char c : t.toCharArray()){
+            if(map.containsKey(c)){
+                map.put(c, map.get(c) - 1);
+            }
+        }
+        for(Map.Entry<Character, Integer> entry : map.entrySet()){
+            if(entry.getValue() > 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
