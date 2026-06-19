605dc8dba8be439a780431358bd72c5029f709e7
Shivang Pathak <shivangpathak11@gmail.com>
2026-06-19 21:36:01 +0530

    Time: 6 ms (5.86%), Space: 43 MB (34.47%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c : sentence.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        if(map.size()<26){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
