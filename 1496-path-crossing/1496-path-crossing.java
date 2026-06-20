c07395065c3e14f8a7aa01aa9c932bf983243edd
Shivang Pathak <shivangpathak11@gmail.com>
2026-06-20 12:56:40 +0530

    Time: 3 ms (79.79%), Space: 43.5 MB (55.9%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.java
+++ 1496-path-crossing/1496-path-crossing.java
@@ -1,22 +1,19 @@
-import java.awt.Point;
-import java.util.*;
-
-
 class Solution {
     public boolean isPathCrossing(String path) {
         int x=0,y=0;
-        Set<Point> set=new HashSet<>();
-        set.add(new Point(x,y));
+        HashSet<String> set=new HashSet<>();
+        set.add("0,0");
         for(char c:path.toCharArray()){
             if(c=='N') y++;
             else if(c=='S') y--;
             else if(c=='W') x--;
             else if(c=='E') x++;
-            Point p=new Point(x, y);
-            if(set.contains(p)){
+
+            String s=x+","+y;
+            if(set.contains(s)){
                 return true;
             }
-            set.add(p);
+            set.add(s);
         }
         return false;
     }
