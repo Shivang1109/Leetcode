7a1fbdf03de6e71d660898d0ed4ef2c5cb75016f
Shivang Pathak <shivangpathak11@gmail.com>
2026-06-20 11:30:52 +0530

    Time: 0 ms (100%), Space: 43 MB (50.77%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder sb = new StringBuilder();
+        int i=0;
+        while(i<command.length()) {
+            char ch = command.charAt(i);
+            if(ch=='G') {
+                sb.append('G');
+                i++;
+            }
+            else if(ch=='(' && command.charAt(i+1)==')') {
+                sb.append('o');
+                i+=2;
+            }
+            else if(ch=='(' && command.charAt(i+1)=='a') {
+                sb.append("al");
+                i+=4;
+            }
+        }
+        return sb.toString();
+    }
+}
