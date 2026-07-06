1652732b1eb8d5125608d9c260d14ee0a72fd89b
Shivang Pathak <shivangpathak11@gmail.com>
2026-07-06 21:16:25 +0530

    Time: 1 ms (97.66%), Space: 42.7 MB (22.97%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<=1) return x;
+        int s=0;
+        int e=x;
+        int res=-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            //mid*mid==x
+            if(mid==x/mid) return mid;
+            //mid*mid>x
+            else if(mid>x/mid) e=mid-1;
+            //mid*mid<x
+            //store the mid and move right to check if any other solutiuon exists.
+            else{
+                res=mid;
+                s=mid+1;
+            }
+        }
+        return res;   
+    }
+}
