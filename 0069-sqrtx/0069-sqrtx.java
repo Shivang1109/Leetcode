2005bcef0ccc8c2e509ae4ece3fad13cd43f7bb2
Shivang Pathak <shivangpathak11@gmail.com>
2026-07-09 01:37:20 +0530

    Time: 1 ms (97.64%), Space: 42.6 MB (61.18%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -6,12 +6,13 @@ class Solution {
         int res=-1;
         while(s<=e){
             int mid=s+(e-s)/2;
+            //mid * mid==x is not used as integer overflow can happen
             //mid*mid==x
             if(mid==x/mid) return mid;
             //mid*mid>x
             else if(mid>x/mid) e=mid-1;
             //mid*mid<x
-            //store the mid and move right to check if any other solutiuon exists.
+            //store the mid and move right to check if any other solution exists.
             else{
                 res=mid;
                 s=mid+1;
