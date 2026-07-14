bffbee7bc7bda5de93a3945b61c4d5c8421a759e
Shivang Pathak <shivangpathak11@gmail.com>
2026-07-14 14:32:20 +0530

    Time: 45 ms (91.12%), Space: 91.2 MB (41.06%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
@@ -0,0 +1,33 @@
+class Solution {
+    public static boolean isValid(int[] arr, long mid, int noOfTrips){
+        long total=0;
+        for(int i:arr){
+            long temp=mid/i;
+            total+=temp;
+            if(total>=noOfTrips){
+                return true;
+            }
+        }
+        return false;
+    }
+    public long minimumTime(int[] time, int totalTrips) {
+        int min=Integer.MAX_VALUE;
+        for(int i:time){
+            min=Math.min(min,i);
+        }
+        long s=0;
+        long e=(long)min*totalTrips;
+        long ans=-1;
+        while(s<=e){
+            long mid=s+(e-s)/2;
+            if(isValid(time,mid,totalTrips)){
+                ans=mid;
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+        }
+        return ans;
+    }
+}
