f08a31372692ed2766b62d8c0861e66e31e482cd
Shivang Pathak <shivangpathak11@gmail.com>
2026-07-17 11:29:42 +0530

    Time: 0 ms (100%), Space: 52.8 MB (77.77%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -3,27 +3,36 @@ class Solution {
         int n=nums.length;
         int s=0;
         int e=n-1;
-        while(s<e){
-            int mid= s+(e-s)/2;
-            if(mid%2==0){
-                if(nums[mid]==nums[mid+1]){
-                    s=mid+2;
-                }
-                else{
-                    e=mid;
-                }
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            int prev=-1;
+            int next=-1;
+            if(mid-1>=0){
+                prev=nums[mid-1];
             }
-            else{
-                if(nums[mid]==nums[mid-1]){
+            if(mid+1<n){
+                next=nums[mid+1];
+            }
+            if(nums[mid]!=prev && nums[mid]!=next){
+                return nums[mid];
+            }
+            else if(nums[mid]==prev && nums[mid]!=next){
+                if((mid&1)==1){
                     s=mid+1;
                 }
                 else{
-                    e=mid;
+                    e=mid-1;
+                }
+            }
+            else if(nums[mid]!=prev && nums[mid]==next){
+                if((mid&1)==1){
+                    e=mid-1;
+                }
+                else{
+                    s=mid+1;
                 }
             }
         }
-        return nums[s];
-        
-        
+        return -1;
     }
 }
