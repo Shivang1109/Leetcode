801dfffcd83fcf54ccc74f3309cfdb0d165fa910
Shivang Pathak <shivangpathak11@gmail.com>
2026-07-18 13:31:38 +0530

    Time: 4 ms (92.77%), Space: 44 MB (92.18%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,16 +1,34 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] arr=s.trim().split(" ");
-        StringBuilder sb=new StringBuilder();
-        int n=arr.length;
-        for(int i=n-1;i>=0;i--) {
-            if (!arr[i].equals("")) {
-                sb.append(arr[i]).append(" ");
+        StringBuilder sb =  new StringBuilder();
+        int i = s.length()-1;
+        while(i>=0){
+            //remove trailing spaces
+            while(i>=0 && s.charAt(i)==' '){
+                i--;
             }
+            if(i<0){
+                break;
+            }
+            int j=i;
+            // j ko word ke phle index pe le jao
+            while(j>=0 && s.charAt(j)!=' '){
+                j--;
+            }
+            //ans me add kr do 
+            sb.append(s.substring(j+1,i+1));
+            //beech ke trailing spaces ko hatao
+            while(j>=0 && s.charAt(j)==' '){
+                j--;
+            }
+            //agar last word nahi hai to space add krdo;
+            if(j>=0){
+                sb.append(' ');
+            }
+            //ab waps se i ko j ke jagah pe le jao ;
+            i=j;
         }
-         String s2=sb.toString().trim();
-        return s2;
-
+        return sb.toString();
         
     }
 }
