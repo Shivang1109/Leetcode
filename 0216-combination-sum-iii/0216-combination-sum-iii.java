dc4e450356e32890800db3422946b7547b5a2cec
Shivang Pathak <shivangpathak11@gmail.com>
2026-07-31 23:47:03 +0530

    Time: 1 ms (22.78%), Space: 42.7 MB (13.2%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -0,0 +1,32 @@
+class Solution {
+    static void solve(int i, int k, int n, int[] arr, List<Integer> res, List<List<Integer>> ans){
+        if(res.size()==k && n==0){
+            ans.add(new ArrayList(res));
+            return;
+        }
+        if(n<0) return;
+        if(i>=arr.length) return;
+
+        res.add(arr[i]);
+        solve(i+1,k,n-arr[i],arr,res,ans);
+        res.remove(res.size()-1);
+        solve(i+1,k,n,arr,res,ans);
+
+
+
+        
+
+    }
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        int i=0;
+        List<Integer> res= new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        int[] arr={1,2,3,4,5,6,7,8,9};
+        solve(i,k,n,arr,res,ans);
+        return ans;
+
+
+        
+        
+    }
+}
