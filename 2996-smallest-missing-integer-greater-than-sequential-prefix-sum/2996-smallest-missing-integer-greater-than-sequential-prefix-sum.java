a75c732937c2a88a97d5a06be9d1f2469318a9fd
Shivang Pathak <shivangpathak11@gmail.com>
2026-08-11 18:43:52 +0530

    Time: 1 ms (93.72%), Space: 44.2 MB (50.35%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -1,43 +1,23 @@
 class Solution {
-    public int BinarySearch(int arr[],int key){
-        int left=0;
-        int right=arr.length-1;
-        while(left<=right){
-            int mid=left+ (right-left)/2;
-            if(key==arr[mid]){
-                return 1;
-            }
-            else if(key>arr[mid]){
-                left=mid+1;
-            }
-            else{
-                right=mid-1;
-            }
-        }
-        return 0;
-
-    }
     public int missingInteger(int[] nums) {
         int sum=nums[0];
-        int len=nums.length;
-        for(int i=1;i<len;i++){
+        int n=nums.length;
+        for(int i=1;i<n;i++){
             if(nums[i]-nums[i-1]==1){
-            sum+=nums[i];
-        }
-        else{
-            break;
-        }
-        }
-        Arrays.sort(nums);
-        int result=BinarySearch(nums,sum);
-        while(result!=0){
-                sum++;
-                result=BinarySearch(nums,sum);
+                sum+=nums[i];
             }
-            return sum;
+            else{
+                break;
+            }
         }
-
-        
-
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<n;i++){
+            set.add(nums[i]);
+        }
+        while(set.contains(sum)){
+            sum++;
+        }
+        return sum;
         
     }
+}
