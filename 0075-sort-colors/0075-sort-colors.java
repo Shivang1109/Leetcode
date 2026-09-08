413c2ee8caf191cc607fc906298efebd56dac7d6
Shivang Pathak <shivangpathak11@gmail.com>
2026-09-08 10:35:30 +0530

    Time: 0 ms (100%), Space: 43.7 MB (22.78%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,28 +1,18 @@
 class Solution {
-
-    static void swap(int[] arr, int i, int j){
-        int temp= arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
     public void sortColors(int[] nums) {
-        int i=0;
-        int j=0;
-        int k=nums.length-1;
-        while(j<=k){
-            if(nums[j]==0){
-                swap(nums,i,j);
-                i++;
-                j++;
-            }
-            else if(nums[j]==1){
-                j++;
-            }
-            else if(nums[j]==2){
-                swap(nums,j,k);
-                k--;
-            }
-        }
+        //counting sort approach;
+
+        int[] freq = new int[]{0,0,0};
         
+        for(int num:nums){
+            freq[num]++;
+        }
+        int k = 0;
+        for(int i=0;i<3;i++){
+            while(freq[i]>0){
+                nums[k++]=i;
+                freq[i]--;
+            }
+        } 
     }
 }
