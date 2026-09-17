db4d8531c3ae199fccfebacca5bace5c01244630
Shivang Pathak <shivangpathak11@gmail.com>
2026-09-17 13:21:21 +0530

    Time: 35 ms (5.64%), Space: 120.7 MB (6.03%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -16,30 +16,25 @@ class Solution {
             len++;
             temp = temp.next;
         }
-        int[] arr = new int[len]; 
-        int k = 0;
+        ArrayList<Integer> arr = new ArrayList<>();
         ListNode curr = head.next;
         ListNode prev = head;
         int count = 1;
         while(curr.next!=null){
             if((curr.val > prev.val && curr.val > curr.next.val) || (curr.val < prev.val && curr.val < curr.next.val)){
-               arr[k++] = count;
+               arr.add(count);
             }
             prev = prev.next;
             curr = curr.next;
             count++;
         }
-        // for(int i:arr){
-        //     System.out.println(i);
-        // }
-        if(k==0 || k==1) return new int[]{-1,-1};
-        int max = arr[k-1] - arr[0];
+        System.out.println(arr);
+        if(arr.size() < 2) return new int[]{-1,-1};
+        int max = arr.get(arr.size()-1) - arr.get(0);
         int min = Integer.MAX_VALUE;
-        for(int i=1;i<k;i++){
-            min = Math.min(min, arr[i]-arr[i-1]);
+        for(int i=1;i<arr.size();i++){
+            min = Math.min(min, arr.get(i)-arr.get(i-1));
         }
-        return new int[]{min,max};
-
-        
+        return new int[]{min,max};    
     }
 }
