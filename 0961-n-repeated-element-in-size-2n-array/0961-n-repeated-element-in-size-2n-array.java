d951fd337e64a00f750a33051b2857e0e79ab758
Shivang Pathak <shivangpathak11@gmail.com>
2026-09-18 20:59:49 +0530

    Time: 0 ms (100%), Space: 47.5 MB (56.47%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i:nums){
+            if(set.contains(i)){
+                return i;
+            }
+            else{
+                set.add(i);
+            }
+        }
+        return -1;
+    }
+}
