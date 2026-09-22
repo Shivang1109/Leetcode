78464f8033c413c9c2255c3899596d1c5029102a
Shivang Pathak <shivangpathak11@gmail.com>
2026-09-23 02:09:20 +0530

    Time: 10 ms (49.23%), Space: 59.3 MB (61.85%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,28 +9,61 @@
  * }
  */
 class Solution {
-    public ListNode sortList(ListNode head) {
-        //BRUTEFORCE APPROACH
-        ListNode temp = head;
-        int count = 0;
-        while(temp != null){
-            count++;
-            temp = temp.next;
+    static ListNode middle(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
         }
-        int[] arr = new int[count];
-        temp = head;
-        for(int i=0;i<count;i++){
-            arr[i] = temp.val;
-            temp = temp.next;
+        return slow;
+
+    }
+    static ListNode merge(ListNode left, ListNode right){
+        ListNode list1 = left;
+        ListNode list2 = right;
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while(list1!=null && list2!=null){
+            if(list1.val <= list2.val){
+                temp.next = list1;
+                list1 = list1.next;
+                temp = temp.next;
+            }
+            else{
+                temp.next = list2;
+                list2 = list2.next;
+                temp = temp.next;
+            }
         }
-        Arrays.sort(arr);
-        temp = head;
-        int j = 0;
-        while(temp != null){
-            temp.val = arr[j++];
-            temp = temp.next;
+        if(list1!=null){
+            temp.next = list1;
         }
-        return head;
+        if(list2!=null){
+            temp.next = list2;
+        }
+        return dummy.next;
+
+    }
+    static ListNode mergeSort(ListNode head){
+        if(head == null || head.next == null) return head;
+        ListNode mid = middle(head);
+        ListNode right = mid.next;
+        mid.next = null;
+        ListNode left = head;
+        left = mergeSort(left);
+        right = mergeSort(right);
+
+        return merge(left,right);
+
         
+    }
+    public ListNode sortList(ListNode head) {
+        // MERGE SORT APPROACH
+        head = mergeSort(head);
+        return head;
+
+    
     }
 }
